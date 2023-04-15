bf0aff27f32625609538559783557691337c2095
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-04-15 18:24:57 -0300

    feat: size footer

--- src/components/ResultCPF/index.tsx
+++ src/components/ResultCPF/index.tsx
@@ -6,7 +6,7 @@ interface ResultCPFProps {
 
 export const ResultCPF = ({ validCPF }: ResultCPFProps) => {
   return (
-    <>
+    <div>
       {validCPF ? (
         <div className="max-[640px]:w-full flex gap-2 items-center mt-4 bg-green-700 rounded p-2 w-80">
           <Info size={24} />O CPF é válido
@@ -16,6 +16,6 @@ export const ResultCPF = ({ validCPF }: ResultCPFProps) => {
           <Info size={24} />O CPF é inválido
         </div>
       )}
-    </>
+    </div>
   );
 };
